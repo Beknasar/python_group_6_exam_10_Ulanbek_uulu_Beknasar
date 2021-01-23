166e814a968a1f5e510d3e33d3030dd1f9452f00
Beknasar <680633@gmail.com>
2021-01-23 17:39:29 +0600

    Добавлена модель для сообщений. Модель добавлен в админку. Добавлена форма для сообщений.

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,9 +1,13 @@
 from django import forms
-
+from webapp.models import Messages
 
 class SearchForm(forms.Form):
     search = forms.CharField(max_length=100, required=False, label="Найти")
 
 
+class MessageForm(forms.ModelForm):
+    class Meta:
+        model = Messages
+        fields = ['message']
 
 
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from accounts.models import Profile
 
-# Create your models here.
+
+class Messages(models.Model):
+    from_user = models.ForeignKey(Profile, related_name='from_user', on_delete=models.CASCADE, verbose_name='от_пользователя')
+    to_user = models.ForeignKey(Profile, related_name='to_user', on_delete=models.CASCADE, verbose_name='к_пользователю')
+    pub_date = models.DateTimeField(auto_now_add=True)
+    message = models.TextField(max_length=2000,  verbose_name='Текст сообщения')
+
+    class Meta:
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
